0708bd849d00df05a00fa2024efdb0b57a04b150
CringeMeister <84626613+Anuiel@users.noreply.github.com>
2022-03-21 20:00:55 +0300

    Update Substring.cpp

--- Substring.cpp
+++ Substring.cpp
@@ -1,158 +1,120 @@
 /*
     Find the longest substring of n string. Time complexity is a very good question, but it is fast, trust me
 */
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define int long long
+const int ALF = 26;
 
-const int MAXN = 120, ALF = 26;
-
-int go[MAXN][ALF], suff[MAXN], supersuff[MAXN], last = 0;
-bitset<MAXN> terminal;
-
-inline void addString(const string &s) {
-    int v = 0;
-    for (int i = 0; i < s.size(); i++) {
-        if (!go[v][s[i] - 'a']) {
-            go[v][s[i] - 'a'] = ++last;
-        }
-        v = go[v][s[i] - 'a'];
+struct Node {
+    int go[ALF], suff, par;
+    int cnt, last_used;
+    int max_len, par_len, char_len;
+    int why;
+    Node() {
+        memset(go, 255, sizeof(go));
+        suff = par = -1;
+        cnt = last_used = 0;
+        max_len = 0;
+        par_len = char_len = why = -1;
     }
-    terminal[v] = true;
-}
+};
 
-inline void precalc() {
-    deque <int> q;
-    q.emplace_back(0);
-    while (!q.empty()) {
-        int v = q.front();
-        q.pop_front();
-        if (v) {
-            if (terminal[suff[v]]) {
-                supersuff[v] = suff[v];
-            } else {
-                supersuff[v] = supersuff[suff[v]];
+vector<Node> nd;
+int last = 1;
+
+inline int add(int A, int ch) {
+    int B = nd.size();
+    nd.push_back(Node());
+    nd[B].par = A;
+    nd[B].suff = 0;
+    for (; A != -1; A = nd[A].suff) {
+        if (nd[A].go[ch] == -1) {
+            if (nd[A].max_len + 1 > nd[B].max_len) {
+                nd[B].max_len = nd[A].max_len + 1;
+                nd[B].par_len = A;
+                nd[B].why = nd[A].why;
+                nd[B].char_len = ch;
             }
+            nd[A].go[ch] = B;
+            continue;
         }
+        int C = nd[A].go[ch];
+        if (nd[C].par == A) {
+            nd[B].suff = C;
+            break;
+        }
+        int D = nd.size();
+        nd.push_back(Node());
         for (int c = 0; c < ALF; c++) {
-            if (!go[v][c]) {
-                go[v][c] = go[suff[v]][c];
-            } else {
-                if (v) {
-                    suff[go[v][c]] = go[suff[v]][c];
-                }
-                q.emplace_back(go[v][c]);
+            nd[D].go[c] = nd[C].go[c];
+        }
+        nd[D].suff = nd[C].suff;
+        nd[C].suff = D;
+        nd[B].suff = D;
+        nd[D].par = A;
+        for (; A != -1 && nd[A].go[ch] == C; A = nd[A].suff) {
+            if (nd[A].max_len + 1 > nd[D].max_len) {
+                nd[D].max_len = nd[A].max_len + 1;
+                nd[D].par_len = A;
+                nd[D].char_len = ch;
+                nd[D].why = nd[A].why;
             }
+            nd[A].go[ch] = D;
         }
+        break;
     }
+    return nd[nd[B].par].go[ch];
 }
 
-typedef vector<vector<int>> matrix;
-
-matrix create(int n, int m) {
-    matrix a;
-    a.resize(n, vector<int>(m, 0));
-    return a;
-}
-
-istream &operator>>(istream &in, matrix &a) {
-    int n = a.size(), m = a[0].size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            in >> a[i][j];
-        }
+void subautomaton(int v) {
+    if (nd[v].last_used == last) {
+        return;
     }
-    return in;
-}
-
-ostream &operator<<(ostream &out, const matrix &a) {
-    int n = a.size(), m = a[0].size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            out << a[i][j] << " ";
-        }
-        out << "\n";
+    nd[v].last_used = last;
+    nd[v].cnt++;
+    if (nd[v].par != -1) {
+        subautomaton(nd[v].par);
     }
-    return out;
-}
 
-const int MOD = 10000;
-
-matrix operator*(matrix &a, const matrix &b) {
-    int n = a.size(), m = b[0].size(), p = a[0].size();
-    matrix c = create(n, m);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            for (int k = 0; k < p; k++) {
-                c[i][j] += 1LL * a[i][k] * b[k][j] % MOD;
-                c[i][j] %= MOD;
-            }
-        }
-    }
-    return c;
-}
-
-void operator*=(matrix &a, const matrix &b) {
-    a = (a * b);
-}
-
-void binPow(matrix &a, long long n) {
-    matrix ans = a;
-    --n;
-    while (n) {
-        if (n & 1) {
-            ans *= a;
-        }
-        a *= a;
-        n >>= 1;
-    }
-    a = ans;
-}
-
-int binPow(int a, long long n, int m = MOD) {
-    if (n == 0) {
-        return 1;
-    }
-    int ans = binPow(a, n / 2, m);
-    if (n % 2 == 0) {
-        return ans * ans % m;
-    } else {
-        return (ans * ans % m) * a % m;
+    if (nd[v].suff != -1) {
+        subautomaton(nd[v].suff);
     }
 }
 
 signed main() {
-    #ifndef LOCAL
-    freopen("strings.in", "r", stdin);
-    freopen("strings.out", "w", stdout);
-    #endif // LOCAL
-    int sz, n;
-    cin >> sz >> n;
-    while (n--) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    int n;
+    cin >> n;
+    nd.push_back(Node());
+    int root[n] = {};
+    for (int i = 0; i < n; i++) {
         string s;
         cin >> s;
-        addString(s);
-    }
-    precalc();
-    matrix base = create(last + 1, last + 1);
-    for (int v = 0; v <= last; v++) {
-        if (terminal[v] || supersuff[v]) {
-            continue;
-        }
-        for (int c = 0; c < ALF; c++) {
-            if (!terminal[go[v][c]] && !supersuff[go[v][c]])
-                base[v][go[v][c]]++;
+        for (auto &c : s) {
+            root[i] = add(root[i], c - 'a');
         }
     }
-    binPow(base, sz);
-    int ans = binPow(ALF, sz);
-    for (int v = 0; v <= last; v++) {
-        if (!terminal[v]) {
-            ans += MOD - base[0][v];
-            ans %= MOD;
+    for (int i = 0; i < n; i++) {
+        subautomaton(root[i]);
+        last++;
+    }
+    int v = 0;
+    for (int i = 0; i < nd.size(); i++) {
+        if (nd[i].cnt == n && (nd[i].max_len > nd[v].max_len || (nd[i].max_len == nd[v].max_len && nd[i].why < nd[v].why))) {
+            v = i;
         }
     }
-    cout << ans;
+    vector<char> ans;
+    while (nd[v].char_len != -1) {
+        ans.push_back(static_cast<char>(nd[v].char_len + 'a'));
+        v = nd[v].par_len;
+    }
+    reverse(ans.begin(), ans.end());
+    for (auto &c : ans) {
+        cout << c;
+    }
 }
